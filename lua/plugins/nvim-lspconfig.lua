5c7f49dab21cc7638600a1c4c83349ac5f78ccbb
Ajie Pangestu <paangestu@gmail.com>
2022-10-08 09:26:16 +0700

    remove unused nvim lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,38 +3,45 @@ if not status_ok then
     return
 end
 
--- Mappings.
 local opts = { noremap = true, silent = true }
+-- Show error
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+
+-- Goto next error
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+
+-- Goto previous error
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+
+-- Show all error
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+local on_attach = function(_, bufnr)
+    local buffopts = { noremap = true, silent = true, buffer = bufnr }
 
-    -- Mappings.
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    -- Create new directory
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, buffopts)
 
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
+    -- Remove directory
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, buffopts)
 
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+    -- Show documentation
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, buffopts)
+
+    -- Goto definition, <C-o> go back
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, buffopts)
+
+    -- Rename variable
+    vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, buffopts)
+
+    -- Format Code
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, buffopts)
+
+    -- Code action
+    vim.keymap.set('n', '<space>a', vim.lsp.buf.code_action, buffopts)
+
+    -- Goto references of variable
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, buffopts)
 end
 
 local lsp_flags = {
@@ -42,16 +49,16 @@ local lsp_flags = {
 }
 
 local language_servers = {
-    'bashls',
-    'html',
-    'cssls',
-    'yamlls',
-    'jsonls',
-    'marksman',
-    'pyright',
-    'tsserver',
-    'volar',
-    'intelephense',
+    'bashls', -- bash
+    'html', -- html
+    'cssls', -- css
+    'yamlls', -- yaml
+    'jsonls', -- json with comment
+    'marksman', -- markdown
+    'pyright', -- python
+    'tsserver', -- typescript
+    'volar', -- vue
+    'intelephense', -- php
 }
 
 for _, language_server in pairs(language_servers) do
@@ -66,7 +73,9 @@ lspconfig['sumneko_lua'].setup {
     on_attach = on_attach,
     settings = {
         Lua = {
-            diagnostics = { globals = { 'vim' } }
+            diagnostics = {
+                globals = { 'vim' }
+            }
         }
     }
 }
