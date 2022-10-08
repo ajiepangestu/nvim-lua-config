fcabbe5b5bc6a08ace469e2f5f1059730c1e7d02
Ajie Pangestu <paangestu@gmail.com>
2022-10-09 06:32:10 +0700

    remove cmdline in nvim cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -62,7 +62,6 @@ cmp.setup {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'path' },
-        { name = 'buffer' },
-        { name = 'cmdline' }
+        { name = 'buffer' }
     },
 }
