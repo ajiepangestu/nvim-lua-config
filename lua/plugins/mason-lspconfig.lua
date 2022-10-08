7ceee8f3b4f81ec2fd108bacabeb02ee65c899b3
Ajie Pangestu <paangestu@gmail.com>
2022-10-08 17:37:47 +0700

    add mason lspconfig

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,21 @@
+-- LSP Manager
+local status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not status_ok then
+    return
+end
+
+mason_lspconfig.setup {
+    ensure_installed = {
+        'bashls', -- bash
+        'html', -- html
+        'cssls', -- css
+        'sumneko_lua', -- lua
+        'yamlls', -- lua
+        'jsonls', -- json with comment
+        'marksman', -- markdown
+        'pyright', -- python
+        'tsserver', -- typescript
+        'volar', -- vue
+        'intelephense', -- php
+    }
+}
