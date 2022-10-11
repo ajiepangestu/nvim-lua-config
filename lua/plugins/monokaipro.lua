40a42e336aa1c42667a3f421af025a4bd6d1d825
Ajie Pangestu <paangestu@gmail.com>
2022-10-11 07:40:10 +0700

    change filter monokaipro

--- lua/plugins/monokaipro.lua
+++ lua/plugins/monokaipro.lua
@@ -1,8 +1,8 @@
 -- Monokai Pro Theme
-local status_ok, monokaipro = pcall(require, 'monokaipro')
+local status_ok, _ = pcall(require, 'monokaipro')
 if not status_ok then
     return
 end
 
-vim.g.monokaipro_filter = 'spectrum'
+vim.g.monokaipro_filter = 'octogon'
 vim.cmd [[colorscheme monokaipro]]
