34439ad4438d9a8eb3224167f7bb5401aa40d53f
Ajie Pangestu <paangestu@gmail.com>
2023-03-08 22:47:54 +0700

    update deprecated function

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,7 +6,6 @@ end
 
 nvim_tree.setup {
     auto_reload_on_write = true,
-    open_on_setup = true,
     filters = {
         dotfiles = false,
         custom = { '^.git$' }
@@ -26,3 +25,20 @@ nvim_tree.setup {
         adaptive_size = true
     }
 }
+
+local function open_nvim_tree(data)
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not directory then
+        return
+    end
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
