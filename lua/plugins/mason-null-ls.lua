02ee79890ca75806776316eeebcebca225d0890b
Ajie Pangestu <paangestu@gmail.com>
2023-09-23 09:33:24 +0700

    update null ls

---
+++ lua/plugins/mason-null-ls.lua
@@ -0,0 +1,20 @@
+-- Formatter and Linter Manager
+local status_ok, mason_null_ls = pcall(require, 'mason-null-ls')
+if not status_ok then
+    return
+end
+
+mason_null_ls.setup {
+    ensure_installed = {
+        -- Python
+        'pylint',   -- linter
+        'autopep8', -- formatter
+
+        -- JavaScript
+        'eslint_d', -- code actions
+        'prettier', -- formatter
+
+        -- Spell Checker
+        'codespell', -- linter
+    }
+}
