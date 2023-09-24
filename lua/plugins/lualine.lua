3c67d7a960e59bfc958b83fd8a086226508ce5f5
Ajie Pangestu <paangestu@gmail.com>
2023-09-24 13:11:52 +0700

    change symbol

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,12 +7,12 @@ end
 lualine.setup {
     options = {
         component_separators = {
-            left = '',
-            right = ''
+            left = '',
+            right = ''
         },
         section_separators = {
-            left = '',
-            right = ''
+            left = '',
+            right = ''
         }
     },
     sections = {
