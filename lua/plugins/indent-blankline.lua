1e1b6585683c6c1b71a1a70c3c75a3296dc328ec
Ajie Pangestu <paangestu@gmail.com>
2023-09-28 16:31:05 +0700

    update indent blank line

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,21 +1,37 @@
 -- Indent Blankline
-local status_ok, indent_blankline = pcall(require, 'indent_blankline')
+local status_ok, indent_blankline = pcall(require, 'ibl')
 if not status_ok then
     return
 end
 
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
+}
+
+local hooks = require "ibl.hooks"
+
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
 
 indent_blankline.setup {
     show_current_context = true,
     show_current_context_start = true,
-    show_first_indent_level = true,
-    show_trailing_blankline_indent = false,
+    indent = { 
+        highlight = highlight
+    }, 
     filetype_exclude = {
         'lspinfo',
         'packer',
@@ -37,12 +53,5 @@ indent_blankline.setup {
         'quickfix',
         'prompt',
     },
-    char_highlight_list = {
-        'IndentBlanklineIndent1',
-        'IndentBlanklineIndent2',
-        'IndentBlanklineIndent3',
-        'IndentBlanklineIndent4',
-        'IndentBlanklineIndent5',
-        'IndentBlanklineIndent6',
-    },
 }
+
