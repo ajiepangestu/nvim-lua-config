0d5ec9aff86c374e10e148df0c6c567b13cf94a2
Ajie Pangestu <paangestu@gmail.com>
2023-10-09 20:45:17 +0700

    change indent blank line

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -26,9 +26,13 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
+vim.g.rainbow_delimiters = { highlight = highlight }
+
 indent_blankline.setup {
-    indent = { 
-        highlight = highlight
-    }, 
+    scope = { highlight = highlight },
+    indent = {
+        -- highlight = highlight
+    },
 }
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 
